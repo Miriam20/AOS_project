20af6ea9e8f53db176f2ab468d6de42a5a87537b
Miriam20 <miriampeta02@gmail.com>
2020-03-31 19:35:13 +0200

    Added struct cpu_data and error fixed in AdjustQUota

--- adaptive_cpu_schedpol.cc
+++ adaptive_cpu_schedpol.cc
@@ -27,53 +27,59 @@
 #include "bbque/app/working_mode.h"
 #include "bbque/res/binder.h"
 
+#include "bbque/binding_manager.h"
+
 #define MODULE_CONFIG SCHEDULER_POLICY_CONFIG "." SCHEDULER_POLICY_NAME
+#define INITIAL_DEFAULT_QUOTA 100
+
+using namespace std::placeholders;
 
 namespace bu = bbque::utils;
 namespace po = boost::program_options;
 
 namespace bbque { namespace plugins {
+    
 
 // :::::::::::::::::::::: Static plugin interface ::::::::::::::::::::::::::::
 
 void * Adaptive_cpuSchedPol::Create(PF_ObjectParams *) {
-	return new Adaptive_cpuSchedPol();
-}
+    return new Adaptive_cpuSchedPol();
+    }
 
-int32_t Adaptive_cpuSchedPol::Destroy(void * plugin) {
-	if (!plugin)
-		return -1;
-	delete (Adaptive_cpuSchedPol *)plugin;
-	return 0;
-}
+    int32_t Adaptive_cpuSchedPol::Destroy(void * plugin) {
+    if (!plugin)
+        return -1;
+    delete (Adaptive_cpuSchedPol *)plugin;
+    return 0;
+    }
 
-// ::::::::::::::::::::: Scheduler policy module interface :::::::::::::::::::
+    // ::::::::::::::::::::: Scheduler policy module interface :::::::::::::::::::
 
-char const * Adaptive_cpuSchedPol::Name() {
-	return SCHEDULER_POLICY_NAME;
-}
+    char const * Adaptive_cpuSchedPol::Name() {
+    return SCHEDULER_POLICY_NAME;
+    }
 
-Adaptive_cpuSchedPol::Adaptive_cpuSchedPol():
-		cm(ConfigurationManager::GetInstance()),
-		ra(ResourceAccounter::GetInstance()) {
-	logger = bu::Logger::GetLogger(MODULE_NAMESPACE);
-	assert(logger);
-	if (logger)
-		logger->Info("adaptive_cpu: Built a new dynamic object[%p]", this);
-	else
-		fprintf(stderr,
-			FI("adaptive_cpu: Built new dynamic object [%p]\n"), (void *)this);
-}
+    Adaptive_cpuSchedPol::Adaptive_cpuSchedPol():
+        cm(ConfigurationManager::GetInstance()),
+        ra(ResourceAccounter::GetInstance()) {
+    logger = bu::Logger::GetLogger(MODULE_NAMESPACE);
+    assert(logger);
+    if (logger)
+        logger->Info("adaptive_cpu: Built a new dynamic object[%p]", this);
+    else
+        fprintf(stderr,
+            FI("adaptive_cpu: Built new dynamic object [%p]\n"), (void *)this);
+    }
 
 
-Adaptive_cpuSchedPol::~Adaptive_cpuSchedPol() {
+    Adaptive_cpuSchedPol::~Adaptive_cpuSchedPol() {
 
 }
 
 
 SchedulerPolicyIF::ExitCode_t Adaptive_cpuSchedPol::_Init() {
 
-	return SCHED_OK;
+    return SCHED_OK;
 }
 
 
@@ -83,49 +89,78 @@ SchedulerPolicyIF::ExitCode_t Adaptive_cpuSchedPol::_Init() {
 ************ MY CODE*************
 ********************************/
 
+void Adaptive_cpuSchedPol::InitializeCPUData(bbque::app::AppCPtr_t papp)
+{
+    cpu_data.prev_quota = ra->UsedBy(
+        "sys.cpu.pe",
+        papp,
+        0);
+    cpu_data.prev_used = ra->Used("sys.cpu.pe");
+    cpu_data.available = ra->Available("sys.cpu.pe");
+}
+
+ba::AwmPtr_t Adaptive_cpuSchedPol::AdjustQuota(bbque::app::AppCPtr_t papp, ba::AwmPtr_t pawm)
+{
+    auto prof = papp->GetRuntimeProfile();
+
+    if (pawm == nullptr) 
+    {
+        //First round
+        pawm = std::make_shared<ba::WorkingMode>(
+        papp->WorkingModes().size(), "MEDIOCRE", 1, papp);
+        cpu_data.next_quota = INITIAL_DEFAULT_QUOTA;
+    }
+    
+    else
+    {
+        assigned_quota = prof.cpu_usage - prof.cpu_usage * prof.ggap_percent;
+    }
+    
+    pawm->AddResourceRequest(
+        "sys.cpu.pe",
+        cpu_data.next_quota,
+        br::ResourceAssignment::Policy::BALANCED);
+    
+    return pawm;
+    
+}
+    
+
 SchedulerPolicyIF::ExitCode_t
 Adaptive_cpuSchedPol::AssignWorkingMode(bbque::app::AppCPtr_t papp)
-{
-    
-    	ApplicationManager & am(ApplicationManager::GetInstance());
+{  
+    ApplicationManager & am(ApplicationManager::GetInstance());
 
-	if (papp == nullptr) {
-		logger->Error("AssignWorkingMode: null application descriptor!");
-		return SCHED_ERROR;
-	}
+    if (papp == nullptr) {
+        logger->Error("AssignWorkingMode: null application descriptor!");
+        return SCHED_ERROR;
+    }
 
-	// Print the run-time profiling info if running
-	if (papp->Running()) {
-		auto prof = papp->GetRuntimeProfile();
-		logger->Info("AssignWorkingMode: [%s] "
-			"cpu_usage=%d c_time=%d, ggap=%d [valid=%d]",
-			papp->StrId(),
-			prof.cpu_usage,
-			prof.ctime_ms,
-			prof.ggap_percent,
-			prof.is_valid);
-	}
-	// Build a new working mode featuring assigned resources
-	ba::AwmPtr_t pawm = papp->CurrentAWM();
-	if (pawm == nullptr) {
-		pawm = std::make_shared<ba::WorkingMode>(
-			papp->WorkingModes().size(), "MEDIOCRE", 1, papp);
-	}
+    // Print the run-time profiling info if running
+    if (papp->Running()) {
+        auto prof = papp->GetRuntimeProfile();
+        logger->Info("AssignWorkingMode: [%s] "
+            "cpu_usage=%d c_time=%d, ggap=%d [valid=%d]",
+            papp->StrId(),
+            prof.cpu_usage,
+            prof.ctime_ms,
+            prof.ggap_percent,
+            prof.is_valid);
+    }
     
-    // Resource request addition
-	pawm->AddResourceRequest(
-		"sys.cpu.pe",
-		100,
-		br::ResourceAssignment::Policy::BALANCED);
+    ba::AwmPtr_t pawm = papp->CurrentAWM();
+    
+    Adaptive_cpuSchedPol::InitializeCPUData(bbque::app::AppCPtr_t papp);
+    pawm = Adaptive_cpuSchedPol::AdjustQuota(papp, pawm);
     
     // Look for the first available CPU
-	BindingManager & bdm(BindingManager::GetInstance());
-	BindingMap_t & bindings(bdm.GetBindingDomains());
-	auto & cpu_ids(bindings[br::ResourceType::CPU]->r_ids);
+    BindingManager & bdm(BindingManager::GetInstance());
+    BindingMap_t & bindings(bdm.GetBindingDomains());
+    auto & cpu_ids(bindings[br::ResourceType::CPU]->r_ids);
     
     for (BBQUE_RID_TYPE cpu_id : cpu_ids) {
-		logger->Info("AssingWorkingMode: [%s] binding attempt CPU id = %d",
-			papp->StrId(), cpu_id);
+        logger->Info("AssingWorkingMode: [%s] binding attempt CPU id = %d",
+        papp->StrId(), cpu_id);
         
         
         // CPU binding
@@ -133,22 +168,22 @@ Adaptive_cpuSchedPol::AssignWorkingMode(bbque::app::AppCPtr_t papp)
         int32_t ref_num = -1;
         ref_num = pawm->BindResource(br::ResourceType::CPU, R_ID_ANY, cpu_id, ref_num);
 
-		if (ref_num < 0) {
-			logger->Error("AssingWorkingMode: [%s] CPU binding to < %d > failed",
-				papp->StrId(), cpu_id);
-			continue;
-		}
+        if (ref_num < 0) {
+            logger->Error("AssingWorkingMode: [%s] CPU binding to < %d > failed",
+                papp->StrId(), cpu_id);
+            continue;
+        }
         
         // Schedule request
-		ApplicationManager::ExitCode_t am_ret;
-		am_ret = am.ScheduleRequest(papp, pawm, sched_status_view, ref_num);
-		if (am_ret != ApplicationManager::AM_SUCCESS) {
-			logger->Error("AssignWorkingMode: [%s] schedule request failed",
-				papp->StrId());
-			continue;
-		}
-		
-		return SCHED_OK;
+        ApplicationManager::ExitCode_t am_ret;
+        am_ret = am.ScheduleRequest(papp, pawm, sched_status_view, ref_num);
+        if (am_ret != ApplicationManager::AM_SUCCESS) {
+            logger->Error("AssignWorkingMode: [%s] schedule request failed",
+                papp->StrId());
+            continue;
+        }
+            
+        return SCHED_OK;
     }
     
     return SCHED_ERROR;
@@ -164,40 +199,40 @@ Adaptive_cpuSchedPol::AssignWorkingMode(bbque::app::AppCPtr_t papp)
 
 SchedulerPolicyIF::ExitCode_t
 Adaptive_cpuSchedPol::Schedule(
-		System & system,
-		RViewToken_t & status_view) {
+        System & system,
+        RViewToken_t & status_view) {
 
-	// Class providing query functions for applications and resources
-	sys = &system;
+    // Class providing query functions for applications and resources
+    sys = &system;
 
-	// Initialization
-	auto result = Init();
-	if (result != SCHED_OK) {
-		logger->Fatal("Schedule: initialization failed");
-		return SCHED_ERROR;
-	} else {
-		logger->Debug("Schedule: resource status view = %ld",
-			sched_status_view);	
-	}
+    // Initialization
+    auto result = Init();
+    if (result != SCHED_OK) {
+        logger->Fatal("Schedule: initialization failed");
+        return SCHED_ERROR;
+    } else {
+        logger->Debug("Schedule: resource status view = %ld",
+            sched_status_view);	
+    }
 
-	/** 
-	 * INSERT YOUR CODE HERE
-	 **/
+    /** 
+    * INSERT YOUR CODE HERE
+    **/
     auto assign_awm = std::bind(
-		static_cast<ExitCode_t (Adaptive_cpuSchedPol::*)(ba::AppCPtr_t)>
-		(&Adaptive_cpuSchedPol::AssignWorkingMode),
-		this, _1);
+        static_cast<ExitCode_t (Adaptive_cpuSchedPol::*)(ba::AppCPtr_t)>
+        (&Adaptive_cpuSchedPol::AssignWorkingMode),
+        this, _1);
     
     ForEachApplicationToScheduleDo(assign_awm);
-	if (result != SCHED_OK)
-		return result;
+    if (result != SCHED_OK)
+        return result;
     
     logger->Debug("Schedule: done");
-	// Return the new resource status view according to the new resource
-	// allocation performed
-	status_view = sched_status_view;
+    // Return the new resource status view according to the new resource
+    // allocation performed
+    status_view = sched_status_view;
 
-	return SCHED_DONE;
+    return SCHED_DONE;
 }
 
 } // namespace plugins
---
+++ adaptive_cpu_schedpol.h
@@ -0,0 +1,128 @@
+/*
+* Copyright (C) 2016  Politecnico di Milano
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef BBQUE_ADAPTIVE_CPU_SCHEDPOL_H_
+#define BBQUE_ADAPTIVE_CPU_SCHEDPOL_H_
+
+#include <cstdint>
+#include <list>
+#include <memory>
+
+#include "bbque/configuration_manager.h"
+#include "bbque/plugins/plugin.h"
+#include "bbque/plugins/scheduler_policy.h"
+#include "bbque/scheduler_manager.h"
+
+#define SCHEDULER_POLICY_NAME "adaptive_cpu"
+
+#define MODULE_NAMESPACE SCHEDULER_POLICY_NAMESPACE "." SCHEDULER_POLICY_NAME
+
+using bbque::res::RViewToken_t;
+using bbque::utils::MetricsCollector;
+using bbque::utils::Timer;
+
+// These are the parameters received by the PluginManager on create calls
+struct PF_ObjectParams;
+
+struct cpu_data_t
+{
+    uint64_t prev_quota;
+    uint64_t prev_used;
+    uint64_t available;
+    uint64_t next_quota;
+}
+
+namespace bbque { namespace plugins {
+
+class LoggerIF;
+
+/**
+* @class Adaptive_cpuSchedPol
+*
+* Adaptive_cpu scheduler policy registered as a dynamic C++ plugin.
+*/
+class Adaptive_cpuSchedPol: public SchedulerPolicyIF {
+
+public:
+
+    // :::::::::::::::::::::: Static plugin interface :::::::::::::::::::::::::
+
+    /**
+    * @brief Create the adaptive_cpu plugin
+    */
+    static void * Create(PF_ObjectParams *);
+
+    /**
+    * @brief Destroy the adaptive_cpu plugin 
+    */
+    static int32_t Destroy(void *);
+
+
+    // :::::::::::::::::: Scheduler policy module interface :::::::::::::::::::
+
+    /**
+    * @brief Destructor
+    */
+    virtual ~Adaptive_cpuSchedPol();
+
+    /**
+    * @brief Return the name of the policy plugin
+    */
+    char const * Name();
+
+
+    /**
+    * @brief The member function called by the SchedulerManager to perform a
+    * new scheduling / resource allocation
+    */
+    ExitCode_t Schedule(System & system, RViewToken_t & status_view);
+    ExitCode_t AssignWorkingMode(bbque::app::AppCPtr_t papp);
+    ba::AwmPtr_t AdjustQuota(bbque::app::AppCPtr_t papp, ba::AwmPtr_t pawm);
+    Adaptive_cpuSchedPol::InitializeCPUData(bbque::app::AppCPtr_t papp);
+    
+private:
+
+    /** Configuration manager instance */
+    ConfigurationManager & cm;
+
+    /** Resource accounter instance */
+    ResourceAccounter & ra;
+
+    /** System logger instance */
+    std::unique_ptr<bu::Logger> logger;
+    
+    cpu_data_t cpu_data;
+
+    /**
+    * @brief Constructor
+    *
+    * Plugins objects could be build only by using the "create" method.
+    * Usually the PluginManager acts as object
+    */
+    Adaptive_cpuSchedPol();
+
+    /**
+    * @brief Optional initialization member function
+    */
+    ExitCode_t _Init();
+};
+
+} // namespace plugins
+
+} // namespace bbque
+
+#endif // BBQUE_ADAPTIVE_CPU_SCHEDPOL_H_
